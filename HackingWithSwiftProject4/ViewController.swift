0a89cc53db8babf497ef52e93359f14d50ded0cb
Chen Sihan <schen@lcs.on.ca>
2020-03-17 02:14:59 -0400

    add navigation delegate + add a open button and the function when open is tapped + webPage title as the screen title

--- HackingWithSwiftProject4/ViewController.swift
+++ HackingWithSwiftProject4/ViewController.swift
@@ -10,22 +10,53 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
-
+    
     var webView: WKWebView!
     
     override func loadView() {
-       webView = WKWebView()
-       webView.navigationDelegate = self
-       view = webView
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Have to start with https:// for it to process
         let url = URL(string: "https://www.apple.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        
     }
-
-
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        
+        // Apple.com option, Invoke openPage Function when tapped
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        
+        
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        // Cancel the option with a nil handler
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        // an Ipad Method
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    // Load the page
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        // Add webpage title to the title
+        title = webView.title
+    }
 }
 
